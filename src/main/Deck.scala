e227b674886aae746eb9421cf6ef2655bd9bfeb2
Erica van der Hoeff <mitchellvanderhoeff@gmail.com>
2011-10-04 19:36:40 +0200

    first yayy

---
+++ src/main/Deck.scala
@@ -0,0 +1,86 @@
+package main
+
+import util.Random
+
+case class Rank(value:String, index:Int) {
+  override def toString = value
+}
+case class Suit(value:String) {
+  override def toString = value
+}
+case class Card(rank:Rank, suit:Suit) {
+  override def toString = {
+    rank.toString + suit.toString
+  }
+}
+
+
+object Deck {
+  val ranks = List("2","3","4","5","6","7","8","9","T","J","Q","K","A")
+  val suits = List("s","h","c","d")
+  val zippedranks = ranks.zipWithIndex
+  val ordereddeck = suits.flatMap((suit) =>
+      zippedranks.map(ri => Card(Rank(ri._1, ri._2), Suit(suit)))
+    )
+  var cards:List[Card] = ordereddeck
+  var lastlookup:(String, String) = ("","")
+
+  def shuffle() {
+    cards = Random.shuffle(ordereddeck)
+  }
+
+  def deal(amount:Int):List[Card] = {
+    val result = cards.take(amount)
+    cards = cards.drop(amount)
+    result
+  }
+
+  private def removecard(card:Card):Card = {
+    cards = cards.filter(_ != card)
+    card
+  }
+
+  private def lookup(f:(Card => Boolean)):Card = {
+    val fetched = cards.find(c => f(c))
+    if(fetched.isEmpty)
+      throw new IllegalArgumentException("card not found. last lookup: "+lastlookup)
+    return removecard(removecard(fetched.get))
+  }
+
+  private def findcard(rs:(String, String)):Card = {
+    lastlookup = rs
+    rs match {
+      case ("X", "x") =>
+        lookup(c => true)
+      case ("X", _) =>
+        lookup(c => c.suit.value == rs._2)
+      case (_, "x") =>
+        lookup(c => c.rank.value == rs._1)
+      case (_, _) =>
+        lookup(c => c.rank.value == rs._1 && c.suit.value == rs._2)
+    }
+  }
+
+  def search(requests:String):List[Card] = {
+    val requestedcards = requests
+      .split(";")
+      .map(req => req.splitAt(1))
+      .map(rs => (rs._1.toUpperCase, rs._2.toLowerCase))
+    val nonrandoms = requestedcards
+      .filter(rs => rs._1 != "X" && rs._2 != "x")
+      .map(findcard(_))
+      .toList
+    val randoms = requestedcards
+      .diff(nonrandoms)
+      .map(findcard(_))
+      .toList
+    val allcards = nonrandoms ::: randoms
+    cards = cards.diff(allcards)
+    allcards
+  }
+
+  override def toString = {
+    cards.toString
+  }
+}
+
