4365ec1d50e13df2a338f2b61a4c166efeb988fa
Mitchell van der Hoeff <mitchellvanderhoeff@gmail.com>
2012-03-11 22:32:21 +0100

    automatic card generation

--- src/main/Main.scala
+++ src/main/Main.scala
@@ -6,10 +6,46 @@ package main
 
 object Main extends App {
   override def main(args: Array[String]) {
+    Deck.shuffle()
+    val cpu = Deck.deal(2)
+    val player = Deck.deal(2)
+    val flop = Deck.deal(3)
+    val turn = Deck.deal(1)
+    val river = Deck.deal(1)
+
+    val cpuString = cpu.mkString(" ")
+    val playerString = player.mkString(" ")
+    val flopString = flop.mkString(" ")
+    val turnString = turn.mkString(" ")
+    val riverString = river.mkString(" ")
+
+    println("cpu: "+cpuString)
+    println("player: "+playerString)
+    println()
+
     Calculate(
-      List("Kh 5d", "xx xx"),
+      List(cpuString, "xx xx"),
       "xx xx xx xx xx",
       10000)
+    println()
+
+    Calculate(
+      List(cpuString, "xx xx"),
+      flopString + " xx xx",
+      10000)
+    println()
+
+    Calculate(
+      List(cpuString, "xx xx"),
+      flopString + " " + turnString + " xx",
+      10000)
+    println()
+
+    Calculate(
+      List(cpuString, "xx xx"),
+      flopString + " " + turnString + " " + riverString,
+      10000)
+    println()
   }
 
 }
